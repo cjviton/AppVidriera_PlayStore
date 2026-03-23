a8af381be95fdc545e6a8d95301594eaf0cd7dc0
= <=>
2026-03-23 09:36:43 +0100

    mail de registro
    Cambio el mal de registro

--- AppVidriera/screens/Register/Register.js
+++ AppVidriera/screens/Register/Register.js
@@ -24,7 +24,7 @@ export default function Register({ navigation }) {
     }
 
     // 🔒 Validación de dominio corporativo
-    const dominioEmpresa = "@lavidrierarandina.com";
+    const dominioEmpresa = "@vidrierarandina.com";
 
     if (!email.toLowerCase().endsWith(dominioEmpresa)) {
       Alert.alert(
